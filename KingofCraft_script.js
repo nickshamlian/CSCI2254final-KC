dd3fcb653296185b78e4a2626f744b6b51d15dbc
nickshamlian <nickshamlian@gmail.com>
2015-04-30 14:08:23 -0400

    Update KingofCraft_script.js

--- KingofCraft_script.js
+++ KingofCraft_script.js
@@ -3,15 +3,7 @@ var base_path = "https://maps.googleapis.com/maps/api/place/textsearch/json?quer
 var query = "craft%20beer%20in%20boston";
 var key = "AIzaSyBgpzdy85uWW4-eHX1a5OkMkpPUdWx7G5M";
 
-var xmlhttp = new XMLHttpRequest();
 var url = base_path + query + "&key=" + key;
 
-xmlhttp.onreadystatechange = function() {
-    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-        var args = JSON.parse(xmlhttp.responseText);
-        myFunction(args);
-    }
-}
-xmlhttp.open("GET", url, true);
-xmlhttp.send();
+
 </script>

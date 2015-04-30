7292932e15ecd02f011443b7314af46705daa93b
nickshamlian <nickshamlian@gmail.com>
2015-04-30 00:13:41 -0400

    Create KingofCraft_listbeers.php

---
+++ KingofCraft_listbeers.php
@@ -0,0 +1,65 @@
+<?php
+
+function KingofCraft_listBeers() {
+  
+  global $wpdb;
+  $table_name = $wpdb->prefix . "KC_beers";
+  $query = "SELECT * FROM $table_name";
+  $allstamps = $wpdb->get_results($query);
+  
+  if($allbeers){
+    KingofCraft_showBeers($allbeers);
+  } else {
+    echo "<h3>No beers yet, add some!</h3>;
+  }
+}
+
+add_shortcode('KingofCraft_showBeers', 'KingofCraft_listBeers');
+
+function KingofCraft_showBeers($allbeers){
+  create_beer_table_header();
+  foreach($allbeers as $beer) {
+    //echo "<pre>" . print_r($beer) . "</pre";
+    create_beer_table_row($beer);
+  }
+  create_beer_table_footer();
+}
+
+function create_beer_table_header() {
+?>
+
+  <div id="beererror"></div>
+  <table class="beertable">
+    <tr class="beertablerow">
+      <th>Beer Information</th>
+      <th>Description</th>
+    </tr>
+    
+<?php
+}
+
+function create_beer_table_footer() {
+?>
+
+  </table>
+
+<?php
+}
+
+function create_beer_table_row($beer) {
+?>
+
+  <tr class="beertablerow">
+    <td><?php echo $beer->beername . " <br>" .
+                   $beer->beerType . " <br>" .
+                   $beer->beerABV . " <br>" .
+                   $beer->brewery . " <br>" . 
+                   $beer->breweryLoc;
+    ?></td>
+    
+    <td><?php echo $beer->beerComment;?></td>
+  </tr>
+  
+<?php
+}
+  

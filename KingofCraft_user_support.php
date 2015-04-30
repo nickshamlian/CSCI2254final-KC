5a28c5b900c954ec707beb5c5b4bbbb91d259727
nickshamlian <nickshamlian@gmail.com>
2015-04-29 20:19:35 -0400

    Update KingofCraft_user_support.php
    Removed types.

--- KingofCraft_user_support.php
+++ KingofCraft_user_support.php
@@ -13,19 +13,8 @@ function KingofCraft_register_form() {
     <label for="password">Password</label>
     <input type="password" name="password" id="password" class="input" size="25"/>
     
-    <label for="type">Membership Type</label>
-    <?php createmenu("memtype", array("guest", "member")); ?>
-    
   </p>
 <?php
-}
-
-function createmenu($name, $option) {
-  echo "<select name=\"$name\">";
-  foreach($option as $opt){
-    echo "<option value=\"$opt\">$opt</option>";
-  }
-  echo "</select>";
 }
 
 add_action('user_register', 'KingofCraft_user_register');
@@ -34,23 +23,14 @@ function KingofCraft_user_register($user_id) {
   if(isset($_POST['netID'])) {
     $pw=sha1($_POST['password']);
     update_user_meta($user_id, 'username', $_POST['username']);
-    update_user_meta($user_id, 'KC_role', $_POST['memtype']);
     update_user_meta($user_id, 'password', $pw);
   }
 }
 
-function is_user_guest($current_user){
-  $key = 'KC_role';
+function get_username($current_user){
+  $key = 'username';
   $single = true;
-  $user_role = get_user_meta($current_user->ID, $key, $single);
-  return($user_role=='guest');
-}
-
-function is_user_member($current_user){
-  $key = 'KC_role';
-  $single = true;
-  $user_role = get_user_meta($current_user->ID, $key, $single);
-  return($user_role=='member');
+  $username = get_user_meta($current_user->username, $key, $single)
 }
 
 function get_user_pw($current_user){

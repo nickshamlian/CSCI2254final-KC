7170d05f6a11d44e8506a258475939dde155f3af
nickshamlian <nickshamlian@gmail.com>
2015-05-06 12:52:05 -0400

    Update and rename KingofCraft_getLocals.php to KingofCraft_getBars.php

---
+++ KingofCraft_getBars.php
@@ -0,0 +1,34 @@
+ <!DOCTYPE html>
+<html>
+  <head>
+    <title>Place searches</title>
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+    <meta charset="utf-8">
+    <style>
+      html, body, #map-canvas {
+        height: 99%;
+        margin: 0px;
+        padding: 0px
+      }
+    </style>
+
+  </head>
+  <body>
+  <div id="map-canvas"></div>
+  </body>
+</html>
+
+  function getBars() {
+    
+  <?php
+  
+    <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&signed_in=true&libraries=places"></script>
+    <script type = "text/javascript" src = "KingofCraft_bar_script.js"></script>
+    
+  ?>
+  
+  }
+  
+  add_shortcode('KingofCraft_getBars', 'getBars');
+  
+  

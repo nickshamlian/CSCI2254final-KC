68413d57c4c0edd2e7787f9217dbbdfe689eede0
nickshamlian <nickshamlian@gmail.com>
2015-05-06 12:47:48 -0400

    Update and rename KingofCraft_script.js to KingofCraft_bar_script.js
    major changes, added textsearch places api for "bar" search

---
+++ KingofCraft_bar_script.js
@@ -0,0 +1,44 @@
+var map;
+var infowindow;
+
+function initialize() {
+  var pyrmont = new google.maps.LatLng(42.336371, -71.167549);
+
+  map = new google.maps.Map(document.getElementById('map-canvas'), {
+    center: pyrmont,
+    zoom: 15
+  });
+
+  var request = {
+    location: pyrmont,
+    radius: 500,
+    query: 'bar'
+  };
+
+  infowindow = new google.maps.InfoWindow();
+  var service = new google.maps.places.PlacesService(map);
+  service.textSearch(request, callback);
+}
+
+function callback(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      createMarker(results[i]);
+    }
+  }
+}
+
+function createMarker(place) {
+  var placeLoc = place.geometry.location;
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.setContent(place.name);
+    infowindow.open(map, this);
+  });
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
--- KingofCraft_script.js
+++
@@ -1,9 +0,0 @@
-<script>
-var base_path = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=";
-var query = "craft%20beer%20in%20boston";
-var key = "AIzaSyBgpzdy85uWW4-eHX1a5OkMkpPUdWx7G5M";
-
-var url = base_path + query + "&key=" + key;
-
-
-</script>

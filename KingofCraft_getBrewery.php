cb991c7df6ab9171225efb3f8a2df98a8bb77952
nickshamlian <nickshamlian@gmail.com>
2015-05-06 13:33:25 -0400

    Update KingofCraft_getBrewery.php

--- KingofCraft_getBrewery.php
+++ KingofCraft_getBrewery.php
@@ -1,32 +1,12 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Place searches</title>
-    <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
-    <meta charset="utf-8">
-    <style>
-      html, body, #map-canvas {
-        height: 99%;
-        margin: 0px;
-        padding: 0px
-      }
-    </style>
+<?php
 
-  </head>
-  <body>
-  <div id="map-canvas"></div>
-  </body>
-</html>
+function getBrewery() {
+?>
 
-  function getBrewery() {
-    
-  <?php
-  
     <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&signed_in=true&libraries=places"></script>
-    <script type = "text/javascript" src = "KingofCraft_brewery_script.js"></script>
-    
-  ?>
-  
-  }
-  
-  add_shortcode('KingofCraft_getBrewery', 'getBrewery');
+    <script src="KingofCraft_brewery_script.js"></script>
+
+<?php
+}
+
+add_shortcode('KingofCraft_getBrewery', 'getBrewery');

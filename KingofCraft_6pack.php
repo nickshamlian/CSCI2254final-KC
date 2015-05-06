94fcf26e24d0c51912ca6cb5b27bfd6be2d01745
nickshamlian <nickshamlian@gmail.com>
2015-05-06 13:37:14 -0400

    Create KingofCraft_6pack.php

---
+++ KingofCraft_6pack.php
@@ -0,0 +1,64 @@
+<?php
+function KingofCraft_6_pack_options() {
+  global $wpdb;
+  $table_name = $wpdb->prefix . "kc_beer";
+  $query = "SELECT * FROM $table_name";
+  $allbeers = $wpdb->get_results($query);
+  if($allbeers){
+    KingofCraft_showOptions($allbeers);
+  }
+  else {
+    return "<h3>No beers yet, add some!</h3>";
+  }
+}
+
+add_shortcode('KingofCraft_6pack_options', 'KingofCraft_6pack_options');
+
+function KingofCraft_showOptions($allbeers){
+  create_beer_table_header();
+  foreach($allbeers as $beer) {
+    create_beer_table_row($beer);
+  }
+  create_beer_table_footer();
+}
+function create_beer_table_header() {
+?>
+
+  <div id="beererror"></div>
+  <table class="beertable">
+    <tr class="beertablerow">
+      <th>Picture</th>
+      <th>Beer Information</th>
+      <th>Description</th>
+    </tr>
+
+<?php
+}
+function create_beer_table_footer() {
+?>
+
+  </table>
+
+<?php
+}
+function create_beer_table_row($beer) {
+?>
+
+  <tr class="beertablerow">
+  	<?php $source=$beer->beer_image; ?>
+  	<td><?php echo "<img src='$source'>";?></td>
+    <td><?php echo $beer->beername . " <br>" .
+                   $beer->beertype . " <br>" .
+                   $beer->beerABV . "% <br>" .
+                   $beer->brewery
+    ?></td>
+
+    <td><?php echo $beer->beer_description;?></td>
+  </tr>
+  <tr>
+    <form method="post">
+    <input type="submit" name="submit value="Add to your pack!">
+  </tr>
+
+<?php
+}

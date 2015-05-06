fa724ab60fe4001125fcd2631930784973cecd25
nickshamlian <nickshamlian@gmail.com>
2015-05-06 01:38:27 -0400

    Update KingofCraft_listbeers.php

--- KingofCraft_listbeers.php
+++ KingofCraft_listbeers.php
@@ -20,6 +20,7 @@ add_shortcode('KingofCraft_listBeers', 'KingofCraft_listBeers');
 function KingofCraft_showBeers($allbeers){
   create_beer_table_header();
   foreach($allbeers as $beer) {
+    //echo "<pre>" . print_r($beer) . "</pre";
     create_beer_table_row($beer);
   }
   create_beer_table_footer();
@@ -52,7 +53,7 @@ function create_beer_table_row($beer) {
   <tr class="beertablerow">
     <td><?php echo $beer->beername . " <br>" .
                    $beer->beertype . " <br>" .
-                   $beer->beerABV . "% <br>" .
+                   $beer->beerABV . " <br>" .
                    $beer->brewery 
     ?></td>
     
